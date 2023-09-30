eed69863e2a50db4335f1b4409854efc801a60b0
Thainara Onofre <thainaraonofre@outlook.com>
2023-09-29 21:57:46 -0300

    Sending DB, DBException, DbIntegrityException

---
+++ src/main/java/demojdbctest/jdbc/db/DbException.java
@@ -0,0 +1,9 @@
+package demojdbctest.jdbc.db;
+
+public class DbException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DbException(String msg) {
+		super(msg);
+	}
+}

785f01a08b63c68539061562c2f31f8dee04121c
Thainara Onofre <thainaraonofre@outlook.com>
2023-09-29 23:06:30 -0300

    Revert "Revert "Sending DB, DBException, DbIntegrityException""
    This reverts commit c56d704867b54d8cce1c03ccd70adc3d06376845.

---
+++ src/main/java/demojdbctest/jdbc/db/DbIntegrityException.java
@@ -0,0 +1,9 @@
+package demojdbctest.jdbc.db;
+
+public class DbIntegrityException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DbIntegrityException(String msg) {
+		super(msg);
+	}
+}

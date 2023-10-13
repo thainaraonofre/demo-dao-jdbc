8cd031ea7ddfd738822f8bde26368332e6132816
Thainara Onofre <thainaraonofre@outlook.com>
2023-10-13 19:13:09 -0300

    insert DepartmentDao

--- src/main/java/demojdbctest/jdbc/model/dao/DaoFactory.java
+++ src/main/java/demojdbctest/jdbc/model/dao/DaoFactory.java
@@ -2,6 +2,7 @@ package demojdbctest.jdbc.model.dao;
 
 
 import demojdbctest.jdbc.db.DB;
+import demojdbctest.jdbc.model.dao.impl.DepartmentDaoJDBC;
 import demojdbctest.jdbc.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -9,4 +10,7 @@ public class DaoFactory {
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
     }
-}
+
+    public static DepartmentDao createDepartmentDao() { return new DepartmentDaoJDBC(DB.getConnection());}
+    }
+
